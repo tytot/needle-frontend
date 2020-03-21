01d31e881cf81a354da0c9cb91d08d8df1c68f58
Tyler <tylerhlin@gmail.com>
2020-03-21 14:53:00 -0400

    Add auth views and components

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '@/components/Login'
+import Register from '@/components/Register'
+import UserBoard from '../views/UserBoard'
+import Admin from '../views/Admin'
 
 Vue.use(VueRouter)
 
@@ -23,7 +27,78 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register,
+      meta: {
+        guest: true
+      }
+    },
+    {
+      path: '/dashboard',
+      name: 'userboard',
+      component: UserBoard,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: Admin,
+      meta: {
+        requiresAuth: true,
+        is_admin: true
+      }
+    },
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (localStorage.getItem('jwt') == null) {
+      next({
+        path: '/login',
+        params: { nextUrl: to.fullPath }
+      })
+    } else {
+      let user = JSON.parse(localStorage.getItem('user'))
+      if (to.matched.some(record => record.meta.is_admin)) {
+        if (user.is_admin == 1) {
+          next()
+        }
+        else {
+          next({ name: 'userboard' })
+        }
+      } else {
+        next()
+      }
+    }
+  } else if (to.matched.some(record => record.meta.guest)) {
+    if (localStorage.getItem('jwt') == null) {
+      next()
+    }
+    else {
+      next({ name: 'userboard' })
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
