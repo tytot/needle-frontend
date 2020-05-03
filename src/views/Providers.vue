50abace60389139eba7fa957f80928594fc16444
Tyler <tylerhlin@gmail.com>
2020-05-03 19:57:04 -0400

    Sortable and searchable provider table

--- src/views/Providers.vue
+++ src/views/Providers.vue
@@ -2,17 +2,37 @@
   <div>
     <add-provider class="mx-auto" @refresh="load"/>
     <delete-provider @refresh="load"/>
-    <div class="d-flex justify-content-center my-2">
+    <div class="d-flex justify-content-center mt-2 mb-4">
       <b-button variant="light" @click="navigate">Cache</b-button>
     </div>
-    <vue-good-table class="px-4"
+      <b-alert v-model="showDanger" dismissible variant="danger" class="m-3">
+        An error occurred while loading the table.
+      </b-alert>
+    <h2 class="mt-2 d-flex justify-content-center">Provider Table</h2>
+    <b-input-group class="p-3">
+      <b-form-input
+        v-model="filter"
+        debounce="500"
+        type="search"
+        id="filterInput"
+        placeholder="Type to Search"
+      ></b-form-input>
+      <b-input-group-append>
+        <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+      </b-input-group-append>
+    </b-input-group>
+    <b-table class="p-3"
+      striped
+      bordered
+      hover
+      responsive
       id="table"
-      :key="componentKey"
-      :columns="columns"
-      :rows="rows"
-      :search-options="{
-            enabled: true
-        }"
+      :items="rows"
+      :fields="fields"
+      :primary-key="uuid"
+      :busy="isBusy"
+      :filter="filter"
+      @filtered="onFiltered"
     />
   </div>
 </template>
@@ -27,34 +47,40 @@ export default {
   },
   data() {
     return {
-      componentKey: 0,
-      columns: [
+      //componentKey: 0,
+      fields: [
         {
-          label: "Facility",
-          field: "facility"
+          sortable: true,
+          key: "facility"
         },
         {
-          label: "Last Name",
-          field: "last_name"
+          sortable: true,
+          key: "last_name"
         },
         {
-          label: "First Name",
-          field: "first_name"
+          sortable: true,
+          key: "first_name"
         },
         {
           label: "Phone #",
-          field: "phone"
+          sortable: true,
+          key: "phone"
         },
         {
           label: "Email",
-          field: "email"
+          sortable: true,
+          key: "email"
         },
         {
           label: "UUID",
-          field: "uuid"
+          sortable: false,
+          key: "uuid"
         }
       ],
-      rows: []
+      rows: [],
+      isBusy: true,
+      showDanger: false,
+      filter: null,
     };
   },
   created() {
@@ -66,22 +92,38 @@ export default {
       .catch(function(error) {
         console.error(error.response);
       });
+    this.isBusy = false;
   },
   methods: {
     load() {
+      this.isBusy = true;
       this.$http
       .get("http://localhost:3000/providers")
       .then(response => {
         this.rows = response.data;
-        this.componentKey += 1;
+        //this.componentKey += 1;
       })
-      .catch(function(error) {
-        console.error(error.response);
+      .catch(function() {
+        this.showErrorAlert();
       });
+      this.isBusy = false;
     },
     navigate() {
       window.open('http://localhost:3000/XML', '_blank');
-    }
+    },
+    showErrorAlert() {
+      this.showDanger = true;
+    },
+    // onFiltered(filteredItems) {
+    //   // Trigger pagination to update the number of buttons/pages due to filtering
+    //   this.totalRows = filteredItems.length
+    //   this.currentPage = 1
+    // }
   }
 };
 </script>
+<style>
+    table.b-table {
+      font-family: 'Courier New', Courier, monospace
+    }
+</style>
